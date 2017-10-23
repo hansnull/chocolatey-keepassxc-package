b687f155dc43b28f487741cb21c619025f7278e3
hansnull <t-len@gmx.de>
2017-10-23 16:10:41 +0200

    Update to 2.2.2

--- keepassxc/tools/chocolateyinstall.ps1
+++ keepassxc/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'keepassxc'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/keepassxreboot/keepassxc/releases/download/2.2.1/KeePassXC-2.2.1-Win64.exe'
+$url64      = 'https://github.com/keepassxreboot/keepassxc/releases/download/2.2.2/KeePassXC-2.2.2-Win64.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,8 +13,8 @@ $packageArgs = @{
 
   softwareName  = 'KeePassXC'
 
-  checksum64    = '70e183a98aeae790bcd43668498785e9'
-  checksumType64= 'md5'
+  checksum64    = 'ee10ac73bf44ba0d2eb6159b4ad2f065a93f84af36f50a1fe4b12f9c91534211'
+  checksumType64= 'sha256'
 
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/S'
